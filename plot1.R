c115915690e19028156223406f3f2bf7dca6041e
Bruno Bertaccini <bruno.bertaccini@unifi.it>
2016-02-07 10:33:39 +0100

    week1 assignment

---
+++ plot1.R
@@ -0,0 +1,20 @@
+# set the new working directory saving the previous one
+old.dir=getwd()
+setwd(paste(old.dir,"/Coursera/Exploratory Data Analysis/week1",sep=""))
+
+
+# only records we are interested in
+var.names=colnames(read.table("household_power_consumption.txt",sep=";",header=T,nrows=1))
+ElPowCon=read.table("household_power_consumption.txt",sep=";",header=T, skip=66636,nrows=2880)
+colnames(ElPowCon)= var.names
+
+# open png device
+png(filename = "plot1.png", width = 480, height = 480)
+
+with(ElPowCon, hist(Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)"))
+
+# close png device
+dev.off()
+
+
+setwd(old.dir)
---
+++ plot4.R
@@ -0,0 +1,35 @@
+# set the new working directory saving the previous one
+old.dir=getwd()
+setwd(paste(old.dir,"/Coursera/Exploratory Data Analysis/week1",sep=""))
+
+# re-set the aspects of the locale in accordance to the English date-time settings
+Sys.setlocale("LC_TIME", "English")
+
+# only records we are interested in
+var.names=colnames(read.table("household_power_consumption.txt",sep=";",header=T,nrows=1))
+ElPowCon=read.table("household_power_consumption.txt",sep=";",header=T, skip=66636,nrows=2880)
+colnames(ElPowCon)= var.names
+
+ElPowCon$DateTime=strptime(paste(ElPowCon$Date,ElPowCon$Time),"%d/%m/%Y %H:%M:%S")
+
+
+# open png device
+png(filename = "plot4.png", width = 480, height = 480)
+
+par(mfcol=c(2,2))
+
+with(ElPowCon, plot(DateTime,Global_active_power, type="l", xlab="", ylab="Global Active Power"))
+
+with(ElPowCon, plot(DateTime,Sub_metering_1, type="l", xlab="", ylab="Energy sub metering"))
+with(ElPowCon, lines(DateTime,Sub_metering_2, col="red"))
+with(ElPowCon, lines(DateTime,Sub_metering_3, col="blue"))
+legend("topright",var.names[7:9],lty=1,col=c("black","red","blue"))
+
+with(ElPowCon, plot(DateTime,Voltage, type="l", xlab="datetime", ylab="Energy sub metering"))
+
+with(ElPowCon, plot(DateTime,Global_reactive_power, type="l", xlab="datetime", ylab="Global reactive power"))
+
+# close png device
+dev.off()
+
+setwd(old.dir)
